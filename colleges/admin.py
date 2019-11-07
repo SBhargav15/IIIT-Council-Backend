d09be49eb6ee360b4b10537eb69d84d98c10e710
Bhargav <singambhargavram@gmail.com>
2019-11-07 19:07:48 +0530

    Create profile and college models

--- colleges/admin.py
+++ colleges/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from colleges.models import College
 # Register your models here.
+admin.site.register(College)
--- colleges/models.py
+++ colleges/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.dispatch import receiver
+from users.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+	ROLES = (('DC', 'Director'), ('FC', 'Faculty'), ('MG', 'Management'), ('MS', 'Miscellaneous'))
+    
+	user = models.OneToOneField(to=User, on_delete=models.CASCADE, related_name='profile')
+	college = models.ForeignKey('colleges.College', on_delete=models.CASCADE, related_name='people',blank=True, null=True)
+	role = models.CharField(choices=ROLES, max_length=2, default='MS')
+	phone_no = models.CharField(max_length=13, blank=True, null=True)
+
+
+@receiver(models.signals.post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		profile = Profile(user=instance)
+		profile.save()
+
+
+class College(models.Model):
+
+    Name = models.CharField(null=False, blank=False, max_length=100)
+    Address = models.CharField(null=False, blank=False, max_length=100)
+
+    # add more attributes
+
+    def __str__(self):
+        return self.Name
